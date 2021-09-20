33f423c88a6c928f538da8a12e1be1afadf2fd7f
butadpj <butadpj@gmail.com>
2021-09-20 11:27:02 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+let input = document.querySelector('form input');
+let addBtn = document.querySelector('.add-btn');
+
+let inputVal = '';
+
+// Set the inputVal everytime the user inputs something
+input.addEventListener('input', (e) => {
+  inputVal = e.target.value;
+});
+
+const resetInput = () => {
+  // Reset input value and do focus
+  input.value = '';
+  inputVal = '';
+  input.focus();
+};
+
+const todoButtonsEventHandler = (newTodo) => {
+  const doneBtn = newTodo.querySelector(".done-btn");
+  const deleteBtn = newTodo.querySelector(".delete-btn");
+  let isTodoDone = false;
+
+  if (doneBtn) {
+    doneBtn.addEventListener('click', () => {
+      // If doneBtn with text "Done" is click
+      if (!isTodoDone) {
+        newTodo.style.textDecoration = 'line-through';
+        doneBtn.innerText = 'Undo';
+        isTodoDone = true;
+      }
+      // If doneBtn with text "Undo" is click
+      else {
+        newTodo.style.textDecoration = 'none';
+        doneBtn.innerText = 'Done';
+        isTodoDone = false;
+      }
+    });
+  }
+
+  if (deleteBtn) {
+    deleteBtn.addEventListener('click', () => {
+      // Get the current todo (parent element of the button)
+      newTodo.remove();
+    });
+  }
+};
+
+const createElements = () => {
+  let todoList = document.querySelector('.todo__list');
+
+  // Create a todo element with a text based on the input value - li
+  let newTodo = document.createElement('li');
+  newTodo.innerText = `${inputVal} `;
+
+  let buttonsWrapper = document.createElement('div');
+
+  // Create a button element with text "Done"
+  let doneBtn = document.createElement('button');
+  doneBtn.className = 'done-btn btn';
+  doneBtn.innerText = 'Done';
+
+  // Create a button element with text "Delete"
+  let deleteBtn = document.createElement('button');
+  deleteBtn.className = 'delete-btn btn';
+  deleteBtn.innerText = 'Delete';
+
+  todoList.prepend(newTodo);
+  buttonsWrapper.appendChild(doneBtn);
+  buttonsWrapper.appendChild(deleteBtn);
+  newTodo.appendChild(buttonsWrapper);
+
+
+  todoButtonsEventHandler(newTodo); // Handles todo's "Done/Undo" & "Delete" button 
+};
+
+const addTodo = () => {
+  if (!inputVal) {
+    // If input is empty
+    input.focus();
+  } else {
+    createElements(); // Create the todo and its buttons
+    resetInput(); // Reset input value and do focus
+  }
+};
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault(); // Prevent page reload if submit button is pressed
+  addTodo();
+});
